7a4e640d62d0dc4f639cf07621998a58cdf0f302
lukasbuschdev <buschlukas175@gmail.com>
2025-02-09 19:35:29 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'main', pathMatch: 'full' },
+    { path: 'main', component: MainComponent },
+    { path: 'privacy', component: PrivacyComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent },
+];
